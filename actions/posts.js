0ced5fbe4d06ed1d3a21429714ab78cadf0b0211
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-06-23 22:45:13 +0300

    Feat(lib\firebase-storage.js): Configure firebase storage and create a function to upload image while creating post

---
+++ actions/posts.js
@@ -0,0 +1,37 @@
+"use server";
+import { redirect } from "next/navigation";
+import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/firebase-storage";
+// START CREATE POST
+export const createPost = async (prevState, formData) => {
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required.");
+  }
+
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+  const imageUrl = await uploadImage(image);
+  storePost({
+    imageUrl: imageUrl,
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed");
+};
+// END CREATE POST
